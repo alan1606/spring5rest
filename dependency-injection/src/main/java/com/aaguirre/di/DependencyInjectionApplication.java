1a48baaf37bda521d7830347af795166c55609f4
alan1606 <mauri.ar3@gmail.com>
2022-08-27 12:56:30 -0600

    Explicit bean declaration

--- dependency-injection/src/main/java/com/aaguirre/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/aaguirre/di/DependencyInjectionApplication.java
@@ -5,12 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import com.aaguirre.di.profiles.EnvironmentService;
-import com.aaguirre.di.quialifiers.Animal;
-import com.aaguirre.di.quialifiers.Dog;
-import com.aaguirre.di.quialifiers.Nest;
-import com.aaguirre.di.scopes.ExampleScopeService;
+import org.springframework.context.annotation.Bean;
 
 
 
@@ -20,14 +15,16 @@ public class DependencyInjectionApplication {
 	
 	private static final Logger log = LoggerFactory.getLogger(DependencyInjectionApplication.class);
 
+	@Bean
+	public String getApplicationName() {
+		return "My app";
+	}
 	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
-		ExampleScopeService exampleScopeService = context.getBean(ExampleScopeService.class);
-		ExampleScopeService exampleScopeService1 = context.getBean(ExampleScopeService.class);
+		String appName = context.getBean(String.class);
 
-		log.info("Are beans equal {}",exampleScopeService.equals(exampleScopeService1));
-		log.info("Are beans == {}",exampleScopeService == exampleScopeService1);
+		log.info("Application name = {}", appName);
 
 	}
 
