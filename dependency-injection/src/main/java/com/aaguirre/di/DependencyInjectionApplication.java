29add481d1e4853bffc0a5127a41e80d0cb29c5c
alan1606 <mauri.ar3@gmail.com>
2022-08-27 15:42:23 -0600

    Spring Expresion Language SpEL

--- dependency-injection/src/main/java/com/aaguirre/di/DependencyInjectionApplication.java
+++ dependency-injection/src/main/java/com/aaguirre/di/DependencyInjectionApplication.java
@@ -9,6 +9,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 import com.aaguirre.di.autowire.AreaCalculatorService;
 
@@ -26,11 +29,9 @@ public class DependencyInjectionApplication {
 	}
 	
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
-		AreaCalculatorService calculator = context.getBean(AreaCalculatorService.class);
-
-		log.info("Total area = {}", calculator.calcAreas());
-
+		ExpressionParser parser = new SpelExpressionParser();
+		Expression expression = parser.parseExpression("10 + 20");
+		log.info("Result {}", expression.getValue());
 	}
 
 }
